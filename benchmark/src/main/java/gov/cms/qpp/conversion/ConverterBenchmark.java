cb5f3184131e2dcb2f0205c8b76f6477cf6285d5
Clyde Tedrick <iamclyde@gmail.com>
2017-05-05 08:36:31 -0400

    probing with ManagementFactory.getThreadMXBean

--- benchmark/src/main/java/gov/cms/qpp/conversion/ConverterBenchmark.java
+++ benchmark/src/main/java/gov/cms/qpp/conversion/ConverterBenchmark.java
@@ -4,11 +4,15 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
+import com.sun.org.apache.xerces.internal.jaxp.SAXParserFactoryImpl;
+import com.sun.org.apache.xerces.internal.jaxp.validation.XMLSchemaFactory;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
@@ -41,48 +45,58 @@ public class ConverterBenchmark {
 	/**
 	 * State management for tests.
 	 */
-	@State(Scope.Thread)
-	public static class Cleaner {
-		@TearDown(Level.Trial)
-		public void doTearDown() throws IOException {
-			Path fileToDeletePath = Paths.get("valid-QRDA-III.qpp.json");
-//			Path fileToDeletePath = Paths.get("gpro-biggest.qpp.json");
-			Files.delete(fileToDeletePath);
-		}
-	}
+//	@State(Scope.Thread)
+//	public static class Cleaner {
+//		@TearDown(Level.Trial)
+//		public void doTearDown() throws IOException {
+//			Path fileToDeletePath = Paths.get("valid-QRDA-III.qpp.json");
+////			Path fileToDeletePath = Paths.get("gpro-biggest.qpp.json");
+//			Files.delete(fileToDeletePath);
+//		}
+//	}
 
 	/**
 	 * Benchmark qrda file conversion.
 	 *
 	 * @param cleaner State management for conversion runs, ensures that output files are deleted.
 	 */
-	@Benchmark
-	@BenchmarkMode({Mode.Throughput, Mode.AverageTime})
-	public void benchmarkMain(Cleaner cleaner) {
-		ConversionEntry.main("src/main/resources/qrda-files/valid-QRDA-III.xml");
-		//ConversionEntry.main("src/main/resources/qrda-files/gpro-biggest.xml", "-d");
-	}
-
 //	@Benchmark
 //	@BenchmarkMode({Mode.Throughput, Mode.AverageTime})
-//	public void benchmarkValidate() throws IOException, ParserConfigurationException, SAXException {
-//		String language = XMLConstants.W3C_XML_SCHEMA_NS_URI;
-//		SchemaFactory factory = SchemaFactory.newInstance(language);
-//		//Schema schema = factory.newSchema(new File("src/main/resources/qrda-conversion.xsd"));
-//		Schema schema = factory.newSchema(new File("src/main/resources/cda/infrastructure/cda/CDA_SDTC.xsd"));
-//
-//		SAXParserFactory spf = SAXParserFactory.newInstance();
-//		spf.setNamespaceAware(true);
-//		spf.setValidating(false);
-//		spf.setSchema(schema);
-//
-//		SAXParser parser = spf.newSAXParser();
-//
-//		Path path = Paths.get("src/main/resources/qrda-files/valid-QRDA-III.xml");
-//		InputStream historical = new BufferedInputStream(Files.newInputStream(path));
-//
-//		parser.parse(historical, new ConversionHandler());
+//	public void benchmarkMain(Cleaner cleaner) {
+//		ConversionEntry.main("src/main/resources/qrda-files/valid-QRDA-III.xml");
+//		//ConversionEntry.main("src/main/resources/qrda-files/gpro-biggest.xml", "-d");
 //	}
+
+	@Benchmark
+	@BenchmarkMode({Mode.Throughput, Mode.AverageTime})
+	public void benchmarkValidate() throws IOException, ParserConfigurationException, SAXException {
+		String language = XMLConstants.W3C_XML_SCHEMA_NS_URI;
+		ThreadMXBean mxBean = ManagementFactory.getThreadMXBean();
+
+		long time = mxBean.getCurrentThreadCpuTime();
+		//SchemaFactory factory = SchemaFactory.newInstance(language);
+		SchemaFactory factory = new XMLSchemaFactory();
+		System.out.println("After SchemaFactory.newInstance: " + (mxBean.getCurrentThreadCpuTime() - time));
+
+		Schema schema = factory.newSchema(new File("src/main/resources/qrda-conversion.xsd"));
+		//Schema schema = factory.newSchema(new File("src/main/resources/cda/infrastructure/cda/CDA_SDTC.xsd"));
+
+		time = mxBean.getCurrentThreadCpuTime();
+		//SAXParserFactory spf = SAXParserFactory.newInstance();
+		SAXParserFactory spf = new SAXParserFactoryImpl();
+		System.out.println("After SAXParserFactory.newInstance: " + (mxBean.getCurrentThreadCpuTime() - time));
+
+		spf.setNamespaceAware(true);
+		spf.setValidating(false);
+		spf.setSchema(schema);
+
+		SAXParser parser = spf.newSAXParser();
+
+		Path path = Paths.get("src/main/resources/qrda-files/valid-QRDA-III.xml");
+		InputStream historical = new BufferedInputStream(Files.newInputStream(path));
+
+		parser.parse(historical, new ConversionHandler());
+	}
 
 
 
--- java-conversion-tool/src/test/java/gov/cms/qpp/conversion/encode/PlaceholderEncoderTest.java
+++ java-conversion-tool/src/test/java/gov/cms/qpp/conversion/encode/PlaceholderEncoderTest.java
@@ -1,12 +1,20 @@
 package gov.cms.qpp.conversion.encode;
 
+import com.sun.org.apache.xerces.internal.jaxp.SAXParserFactoryImpl;
+import com.sun.org.apache.xerces.internal.jaxp.validation.XMLSchemaFactory;
 import gov.cms.qpp.conversion.encode.helper.RegistryHelper;
 import gov.cms.qpp.conversion.model.Node;
 import gov.cms.qpp.conversion.model.Registry;
 import gov.cms.qpp.conversion.model.TemplateId;
 import gov.cms.qpp.conversion.model.error.ValidationError;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import javax.xml.XMLConstants;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.validation.SchemaFactory;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -16,26 +24,52 @@ import static org.junit.Assert.assertThat;
  * Tests the Placeholder adds coverage for CircleCI
  */
 public class PlaceholderEncoderTest extends PlaceholderEncoder {
+
+
+//	@Test
+//	public void internalEncodeMissingEncoderTest() throws Exception {
+//
+//		Registry<String, JsonOutputEncoder> invalidRegistry =
+//				RegistryHelper.makeInvalidRegistry(PlaceholderEncoder.class.getName());
+//		Registry<String, JsonOutputEncoder> validRegistry = QppOutputEncoder.ENCODERS;
+//
+//		Node root = new Node(TemplateId.DEFAULT.getTemplateId());
+//		Node placeHolderNode = new Node(root, TemplateId.PLACEHOLDER.getTemplateId());
+//		root.addChildNode(placeHolderNode);
+//		JsonWrapper testJsonWrapper = new JsonWrapper();
+//		PlaceholderEncoder placeHolderEncoder = new PlaceholderEncoder();
+//
+//		RegistryHelper.setEncoderRegistry(invalidRegistry); //Set Registry with missing class
+//
+//		placeHolderEncoder.internalEncode(testJsonWrapper, root);
+//		List<ValidationError> errors = placeHolderEncoder.getValidationErrors();
+//		assertThat("Expecting Encode Exception", errors.size(), is(1));
+//
+//		RegistryHelper.setEncoderRegistry(validRegistry); //Restore Registry
+//	}
+
 	@Test
-	public void internalEncodeMissingEncoderTest() throws Exception {
+	public void testTest() {
+		String language = XMLConstants.W3C_XML_SCHEMA_NS_URI;
+		ThreadMXBean mxBean = ManagementFactory.getThreadMXBean();
 
-		Registry<String, JsonOutputEncoder> invalidRegistry =
-				RegistryHelper.makeInvalidRegistry(PlaceholderEncoder.class.getName());
-		Registry<String, JsonOutputEncoder> validRegistry = QppOutputEncoder.ENCODERS;
+		long time = mxBean.getCurrentThreadCpuTime();
+//		System.out.println("Before SchemaFactory.newInstance: " + time);
 
-		Node root = new Node(TemplateId.DEFAULT.getTemplateId());
-		Node placeHolderNode = new Node(root, TemplateId.PLACEHOLDER.getTemplateId());
-		root.addChildNode(placeHolderNode);
-		JsonWrapper testJsonWrapper = new JsonWrapper();
-		PlaceholderEncoder placeHolderEncoder = new PlaceholderEncoder();
+		SchemaFactory factory = SchemaFactory.newInstance(language);
+		System.out.println("After general: " + (mxBean.getCurrentThreadCpuTime() - time));
+		time = mxBean.getCurrentThreadCpuTime();
+		factory = XMLSchemaFactory.newInstance(language);
+		System.out.println("After specific: " + (mxBean.getCurrentThreadCpuTime() - time));
 
-		RegistryHelper.setEncoderRegistry(invalidRegistry); //Set Registry with missing class
+		time = mxBean.getCurrentThreadCpuTime();
+		SAXParserFactory spf = SAXParserFactory.newInstance();
+		System.out.println("After general: " + (mxBean.getCurrentThreadCpuTime() - time));
 
-		placeHolderEncoder.internalEncode(testJsonWrapper, root);
-		List<ValidationError> errors = placeHolderEncoder.getValidationErrors();
-		assertThat("Expecting Encode Exception", errors.size(), is(1));
-
-		RegistryHelper.setEncoderRegistry(validRegistry); //Restore Registry
+		time = mxBean.getCurrentThreadCpuTime();
+		SAXParserFactoryImpl.newInstance();
+		System.out.println("After specific: " + (mxBean.getCurrentThreadCpuTime() - time));
+		time = mxBean.getCurrentThreadCpuTime();
 	}
 
 }
