d1c7390179883864f3edaf527d5280b6c6fbd250
Clyde Tedrick <iamclyde@gmail.com>
2017-05-08 10:31:15 -0400

    xsd validation failure

--- benchmark/src/main/java/gov/cms/qpp/conversion/ConverterBenchmark.java
+++ benchmark/src/main/java/gov/cms/qpp/conversion/ConverterBenchmark.java
@@ -1,18 +1,5 @@
 package gov.cms.qpp.conversion;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.management.ManagementFactory;
-import java.lang.management.ThreadMXBean;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.concurrent.TimeUnit;
-
-import com.sun.org.apache.xerces.internal.jaxp.SAXParserFactoryImpl;
-import com.sun.org.apache.xerces.internal.jaxp.validation.XMLSchemaFactory;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
@@ -30,78 +17,112 @@ import javax.xml.XMLConstants;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-import javax.xml.validation.Schema;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.events.XMLEvent;
 import javax.xml.validation.SchemaFactory;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Performance test harness.
  */
 @Warmup(iterations = 0, time = 1, timeUnit = TimeUnit.MILLISECONDS)
 @Measurement(iterations = 1, time = 1, timeUnit = TimeUnit.MILLISECONDS)
-@Fork(value = 15, jvmArgsPrepend = {"-Xmx50m", "-Xms50m"})
+@Fork(value = 1, jvmArgsPrepend = {"-Xmx50m", "-Xms50m"})
 @Threads(1)
 public class ConverterBenchmark {
+//	private static String filePath = "src/main/resources/qrda-files/valid-QRDA-III.xml";
+//	private static String output = "valid-QRDA-III.qpp.json";
+	private static String filePath = "src/main/resources/qrda-files/gpro-biggest.xml";
+	private static String output = "gpro-biggest.qpp.json";
+	private static String schemaPath = "src/main/resources/cda/infrastructure/cda/CDA_SDTC.xsd";
 
 	/**
 	 * State management for tests.
 	 */
-//	@State(Scope.Thread)
-//	public static class Cleaner {
-//		@TearDown(Level.Trial)
-//		public void doTearDown() throws IOException {
-//			Path fileToDeletePath = Paths.get("valid-QRDA-III.qpp.json");
-////			Path fileToDeletePath = Paths.get("gpro-biggest.qpp.json");
-//			Files.delete(fileToDeletePath);
-//		}
-//	}
+	@State(Scope.Thread)
+	public static class Cleaner {
+		@TearDown(Level.Trial)
+		public void doTearDown() throws IOException {
+			Path fileToDeletePath = Paths.get(output);
+			Files.delete(fileToDeletePath);
+		}
+	}
 
 	/**
 	 * Benchmark qrda file conversion.
 	 *
 	 * @param cleaner State management for conversion runs, ensures that output files are deleted.
 	 */
-//	@Benchmark
-//	@BenchmarkMode({Mode.Throughput, Mode.AverageTime})
-//	public void benchmarkMain(Cleaner cleaner) {
-//		ConversionEntry.main("src/main/resources/qrda-files/valid-QRDA-III.xml");
-//		//ConversionEntry.main("src/main/resources/qrda-files/gpro-biggest.xml", "-d");
-//	}
+	@Benchmark
+	@BenchmarkMode({Mode.Throughput, Mode.AverageTime})
+	public void benchmarkMain(Cleaner cleaner) {
+		ConversionEntry.main(filePath);
+	}
 
 	@Benchmark
 	@BenchmarkMode({Mode.Throughput, Mode.AverageTime})
-	public void benchmarkValidate() throws IOException, ParserConfigurationException, SAXException {
+	public void benchmarkStreamingParser() throws IOException, ParserConfigurationException, SAXException {
 		ThreadMXBean mxBean = ManagementFactory.getThreadMXBean();
 		long time = mxBean.getCurrentThreadCpuTime();
 		String language = XMLConstants.W3C_XML_SCHEMA_NS_URI;
 
 
-		//SchemaFactory factory = SchemaFactory.newInstance(language);
-		SchemaFactory factory = new XMLSchemaFactory();
+		SchemaFactory factory = SchemaFactory.newInstance(language);
+		//SchemaFactory factory = new XMLSchemaFactory();
 		//System.out.println("After SchemaFactory.newInstance: " + (mxBean.getCurrentThreadCpuTime() - time));
 
-		//Schema schema = factory.newSchema(new File("src/main/resources/qrda-conversion.xsd"));
-		Schema schema = factory.newSchema(new File("src/main/resources/cda/infrastructure/cda/CDA_SDTC.xsd"));
+		//Schema schema = factory.newSchema(new File(schemaPath));
 
 		//time = mxBean.getCurrentThreadCpuTime();
-		//SAXParserFactory spf = SAXParserFactory.newInstance();
-		SAXParserFactory spf = new SAXParserFactoryImpl();
+		SAXParserFactory spf = SAXParserFactory.newInstance();
+		//SAXParserFactory spf = new SAXParserFactoryImpl();
 		//System.out.println("After SAXParserFactory.newInstance: " + (mxBean.getCurrentThreadCpuTime() - time));
 
 		spf.setNamespaceAware(true);
 		spf.setValidating(false);
-		spf.setSchema(schema);
+		//spf.setSchema(schema);
 
 		SAXParser parser = spf.newSAXParser();
 
-		Path path = Paths.get("src/main/resources/qrda-files/valid-QRDA-III.xml");
+		Path path = Paths.get(filePath);
 		InputStream historical = new BufferedInputStream(Files.newInputStream(path));
 
 		parser.parse(historical, new ConversionHandler());
 		System.out.println("After: " + (mxBean.getCurrentThreadCpuTime() - time));
 	}
 
-
-
+	@Benchmark
+	@BenchmarkMode({Mode.Throughput, Mode.AverageTime})
+	public void benchmarkPullParse() throws FileNotFoundException, XMLStreamException {
+		Path path = Paths.get(filePath);
+		XMLInputFactory factory = XMLInputFactory.newInstance();
+		XMLStreamReader r =
+				factory.createXMLStreamReader(
+						new BufferedInputStream(
+								new FileInputStream(path.toFile())));
+		while(r.hasNext()) {
+			int eventType = r.next();
+			if (eventType == XMLStreamReader.START_ELEMENT) {
+				String name = r.getLocalName();
+				if ("templateId".equals(name)) {
+					String templateId = r.getAttributeValue("", "root");
+					System.out.println("template Id: " + templateId);
+				}
+			}
+		}
+	}
 
 
 }
